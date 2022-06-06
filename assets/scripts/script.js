eda20a365d29e2a5332c0f7da889bf26ce548c3f
HaythemLazaar <haythem.lazaar1@gmail.com>
2022-06-06 14:07:22 +0100

    First Commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,23 @@
+var menus = document.querySelectorAll(".menu1")
+
+function changeClass(){
+    for(let i=0;i<menus.length;i++){
+        if ( menus[i].className.match(/(?:^|\s)activated(?!\S)/) ){
+            menus[i].classList.remove('activated');
+            menus[i].classList.add('deactivated');
+            document.getElementById("act-btn").src = "assets/img/HAMBURGER.svg";
+        }
+        else{
+            menus[i].classList.remove('deactivated');
+            menus[i].classList.add('activated');
+            document.getElementById("act-btn").src = "assets/img/X.svg";
+            console.log("Active");
+        }
+    }
+    
+}
+
+window.onload = function(){
+    document.getElementById("act-btn1").addEventListener( 'click', changeClass);
+    document.getElementById("act-btn").addEventListener( 'touchend', changeClass);
+}
